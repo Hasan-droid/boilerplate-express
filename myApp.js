f33f9907dcfe4a1e0cfea7009372b44c965e0342
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-06-25 16:52:21 +0300

    make changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,19 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
+console.log("hello world");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// app.get("/", (req, res) => {
+//   res.send("Response String");
+// });
+app.use(express.static(__dirname + "/public/style.css"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    return res.json({ message: "HELLO JSON" });
+  }
+  res.json({ message: "Hello json" });
+});
+module.exports = app;
